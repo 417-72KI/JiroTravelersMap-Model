3d389877efe9f52238b86f62b251f02543af3c70
417-72KI <417.72ki@gmail.com>
2026-07-05 23:09:15 +0900

    add decoding

---
+++ Sources/JiroTravelersMapModel/Decoding.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public extension Shop {
+    static func decode(_ data: Data) throws -> Shop {
+        try decoder.decode(Shop.self, from: data)
+    }
+
+    static func decode(_ data: Data) throws -> [Shop] {
+        try decoder.decode([Shop].self, from: data)
+    }
+}
+
+public extension Shop {
+    static var decoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+}
+
+extension Shop {
+    static var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ja_JP")
+        df.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        df.dateFormat = "yyyy/MM/dd"
+        return df
+    }
+}
--- Tests/JiroTravelersMapModelTests/ShopTests.swift
+++ Tests/JiroTravelersMapModelTests/ShopTests.swift
@@ -5,9 +5,7 @@ import JiroTravelersMapModel
 @Test
 func decode() async throws {
     let input = #"[{"id":1,"kind":"origin","name":"三田本店","status":"open","prefecture":"tokyo","address":"港区三田2-16-4","location":{"lat":35.64806,"lng":139.7416326},"regular_holiday":["sunday","holiday"],"opening_hours":{"monday":[{"start":"09:00","end":"17:00"}],"tuesday":[{"start":"09:00","end":"17:00"}],"wednesday":[{"start":"09:00","end":"17:00"}],"thursday":[{"start":"09:00","end":"17:00"}],"friday":[{"start":"09:00","end":"17:00"}],"saturday":[{"start":"09:00","end":"17:00"}],"sunday":[],"holiday":[]},"has_parking":false,"hard_noodle_enabled":true}]"#
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase
-    let shops = try decoder.decode([Shop].self, from: Data(input.utf8))
+    let shops = try Shop.decode(Data(input.utf8)) as [Shop]
     let shop = try #require(shops.first)
     #expect(shop.name ==  "三田本店")
     #expect(shop.status == .open)
